394d45f3d9d80cfae0a806ae1380dc52d9f4d907
Эфендиев Магомед Арифович <mefendiev@corp.local>
2024-04-04 12:08:24 +0300

    Formatting fix

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -17,7 +17,7 @@ class CommentViewSet(viewsets.ModelViewSet):
         serializer.save(
             author=self.request.user,
             post=self.get_post())
-        
+
     def get_post(self):
         return get_object_or_404(Post, pk=self.kwargs['post_id'])
 
